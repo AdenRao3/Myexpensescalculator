e36b71f85759bd02b53a9f5f0460d287357c93cb
AdenRao3 <47573320+AdenRao3@users.noreply.github.com>
2020-11-12 17:39:39 -0500

    Add files via upload

---
+++ codingcosts.py
@@ -0,0 +1,11 @@
+# This program keeps track of my coding expenses
+
+domain1predictionwebsite = 11.60
+domain2advisingwebsite = 11.60
+playstorecost = 32.82
+appstorecost = 129.98
+
+total = domain1predictionwebsite + domain2advisingwebsite + playstorecost + appstorecost
+
+print("Total cost is: $")
+print(total)#, end=' ')
